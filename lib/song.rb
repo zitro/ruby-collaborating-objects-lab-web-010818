aa5bfb8fbb27d4d626089b74f888c40e17b28a2c
zitro <bryan@bryanortiz.com>
2018-02-03 10:17:53 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -18,12 +18,15 @@ def save
 end
 
 def self.new_by_filename(filename)
-	file_arr = filename.split(' - ')  #[artist, song, genre]
+	file_arr = filename.split(' - ')
+	#[artist, song, genre]
 	song_name = file_arr[1]
 	artist_name = file_arr[0]
+
 	#create new song inst and new artist inst
 	new_song_inst = self.new(song_name)
 	new_artist_inst = Artist.find_or_create_by_name(artist_name)
+
 	#link up new instances to both sides
 	new_song_inst.artist = new_artist_inst
 	new_artist_inst.add_song(new_song_inst)
